d8cceba6f5ff7e5cdb62b6dd95fe338caea83db3
zweiqin <zweiqin88@163.com>
2023-12-09 09:06:49 +0800

    2023.12.09 - 完成小程序项目

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<web-view src="https://h5.jfcmei.com/#/pages/store/store?miniProgram=1"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			console.log('小程序打开')
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/loading/loading.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		1111
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'Loading',
+		data() {
+			return {
+				orderNo: '',
+				userId: '',
+				query: {}
+			}
+		},
+		// '/pages/loading/loading?' + query + 'orderNo=' + data.orderSn + '&userId=' + getUserId()
+		onLoad(options) {
+			const {
+				orderNo,
+				userId,
+				...args
+			} = options
+			this.orderNo = orderNo
+			this.userId = userId
+			this.query = args
+			console.log(options)
+			uni.navigateToMiniProgram({
+				appId: 'wx984383bfd5405df1',
+				path: 'pages/orderDetail/orderDetail?' + this.query,
+				extraData: {},
+				success: (res) => {
+					console.log('打开成功')
+				},
+				fail: (err) => {
+					console.log('打开失败', err)
+				},
+				complete: (result) => {
+					setTimeout(() => {
+						uni.navigateBack()
+					}, 500);
+				}
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style></style>
